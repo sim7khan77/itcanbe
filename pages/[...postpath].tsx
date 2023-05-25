3c30ab424981d765a3398f12da78498561e86baf
Asim9990 <133738091+Asim9990@users.noreply.github.com>
2023-05-25 11:17:23 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://sxhrx.000webhostapp.com/graphql"
+	const endpoint = "https://findofun.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://mosqueworking.com/f34sp36d7b?key=bb75d438f555312b9797d1f7509642f7` 
+					`https://findofun.com/` 
 				}`,
 			},
 		};
